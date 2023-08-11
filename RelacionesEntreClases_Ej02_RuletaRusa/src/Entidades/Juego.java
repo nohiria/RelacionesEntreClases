3e46090a1c34fc7074579a41189633cbd7d1a835
Nohiria Velazquez <nohiriavg@gmail.com>
2023-08-10 19:23:33 -0500

    Ejercicio 02 Ruleta rusa Clase juego y Main

--- RelacionesEntreClases_Ej02_RuletaRusa/src/Entidades/Juego.java
+++ RelacionesEntreClases_Ej02_RuletaRusa/src/Entidades/Juego.java
@@ -5,10 +5,54 @@
  */
 package Entidades;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nohyv
  */
 public class Juego {
+    private ArrayList<Jugador> jugadores;
+    private Revolver revolver;
     
+    //Constructors
+    public Juego(){
+    }
+
+    public Juego(ArrayList<Jugador> jugadores, Revolver revolver) {
+        this.jugadores = jugadores;
+        this.revolver = revolver;
+    }
+    
+    //Getters&Setters
+    public ArrayList<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public Revolver getRevolver() {
+        return revolver;
+    }
+
+    public void setRevolver(Revolver revolver) {
+        this.revolver = revolver;
+    }
+    
+    //Methods
+    public void llenarJuego(ArrayList<Jugador> jugadores, Revolver revolver){
+        this.jugadores=jugadores;
+        this.revolver= revolver;
+    }
+
+    public boolean ronda(int turno){
+        boolean continuarJuego= true;
+        jugadores.get(turno).disparo(revolver);
+        if(jugadores.get(turno).isMojado()){
+            continuarJuego= false;
+        }
+        return continuarJuego;
+    }
 }
--- RelacionesEntreClases_Ej02_RuletaRusa/src/relacionesentreclases_ej02_ruletarusa/RelacionesEntreClases_Ej02_RuletaRusa.java
+++ RelacionesEntreClases_Ej02_RuletaRusa/src/relacionesentreclases_ej02_ruletarusa/RelacionesEntreClases_Ej02_RuletaRusa.java
@@ -25,8 +25,19 @@ Revolver
 Métodos:
 • llenarJuego(ArrayList<Jugador>jugadores, Revolver r): este método recibe los jugadores
 y el revolver para guardarlos en los atributos del juego.
+• ronda(): cada ronda consiste en un jugador que se apunta con el revolver de agua y
+aprieta el gatillo. Sí el revolver tira el agua el jugador se moja y se termina el juego, sino se
+moja, se pasa al siguiente jugador hasta que uno se moje. Si o si alguien se tiene que
+mojar. Al final del juego, se debe mostrar que jugador se mojó.
+Pensar la lógica necesaria para realizar esto, usando los atributos de la clase Juego.
  */
 package relacionesentreclases_ej02_ruletarusa;
+
+import Entidades.Juego;
+import Entidades.Jugador;
+import Entidades.Revolver;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
@@ -37,8 +48,56 @@ public class RelacionesEntreClases_Ej02_RuletaRusa {
     /**
      * @param args the command line arguments
      */
+    
+    private static Scanner input= new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Jugador> jugadores= new ArrayList<>();
+        
+        System.out.println("JUEGO RULETA RUSA");
+        System.out.println("_________________/n/n");
+        
+        //Ingresar jugadores
+        System.out.println("Ingrese la cantidad de jugadores");
+        int cantidadJugadores= Integer.parseInt(input.nextLine());
+        if(cantidadJugadores<2&&cantidadJugadores>6){
+            cantidadJugadores= 6;
+        }
+        
+        for(int i=0; i<cantidadJugadores; i++){ //Crear jugadores asignando el id
+            Jugador jugador= new Jugador(i+1);
+            jugadores.add(jugador);
+        }
+        System.out.println(cantidadJugadores+" jugadores ingresados");
+        
+        System.out.println("Preparando revolver para el juego");
+        Revolver r= new Revolver();
+        r.llenarRevolver();
+        System.out.println("Revolver listo");
+        //System.out.println(r.toString());
+        
+        System.out.println("Se inicia el juego.");
+        Juego juego= new Juego(jugadores, r);
+        
+        boolean continuarJuego= true;
+        int turno= 0;
+        
+        while(continuarJuego){
+            if(turno==cantidadJugadores){
+                turno=0;
+            }
+            
+            System.out.println(jugadores.get(turno).getNombre()+" dispara");
+            continuarJuego=juego.ronda(turno);
+            if(continuarJuego){
+                System.out.println(jugadores.get(turno).getNombre()+" no ha sido mojado");
+                turno++;
+            }else{
+                System.out.println(jugadores.get(turno).getNombre()+" ha sido mojado");
+                System.out.println("Juego terminado");
+                //System.out.println("El agua estaba en la posición "+r.getPosicionAgua());
+            } 
+        }
     }
     
 }
